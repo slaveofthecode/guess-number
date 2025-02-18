aaf24603842961014691098d6862aa0e6358ee4a
Gustavo Lopez <gml.gustavoml@gmail.com>
2025-02-17 23:05:24 -0300

    Vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensures Vercel serves the files correctly
+  build: {
+    outDir: "dist", // Where the production files will be saved
+  },
 })
