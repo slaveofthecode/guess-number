f13877dd0dc9e2bb2cb3af6befa0556ea336a2f3
Gustavo Lopez <gml.gustavoml@gmail.com>
2025-02-19 19:27:44 -0300

    Add Tailwind CSS integration and update components styling

--- src/components/StartGame/index.tsx
+++ src/components/StartGame/index.tsx
@@ -1,7 +1,7 @@
 export default function StartGame() {
 
     return (
-        <div>
+        <div className="flex flex-col bg-blue-500 p-2 w-full gap-2" >
             <h2>Guess Number</h2>
             <input type="text" placeholder="Enter your name" />
             <button>Start Game</button>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   base: "/", // Ensures Vercel serves the files correctly
   build: {
     outDir: "dist", // Where the production files will be saved
