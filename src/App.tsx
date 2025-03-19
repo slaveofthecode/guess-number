e106bcd94c43d99dc6570ec024a4e6d29a339e87
Gustavo Lopez <gml.gustavoml@gmail.com>
2025-03-18 22:41:17 -0300

    My first push notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import OneSignal from 'react-onesignal';
+// Importa tus otros componentes y estilos
 import StartGame from './components/StartGame';
 import Playing from './components/Playing';
 import Header from './components/Header';
@@ -5,6 +8,17 @@ import Header from './components/Header';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // Inicializar OneSignal cuando el componente se monta
+    OneSignal.init({
+      appId: '26b12df5-7d0b-4824-b51e-50ed5471f59f',
+      allowLocalhostAsSecureOrigin: true, // Esto permite pruebas en localhost
+      notifyButton: {
+        enable: true,
+      },
+    });
+  }, []);
+
   return (
     <div>
       <Header />
