1effc103f6f7010b532b036c2aa59eeaba74b8de
Gustavo Lopez <gml.gustavoml@gmail.com>
2025-03-27 23:38:41 -0300

    Remove deprecated OneSignal SDK worker and update service worker to use the latest SDK version

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-// importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
 self.addEventListener('install', () => {
   console.log('Service Worker instalado');
