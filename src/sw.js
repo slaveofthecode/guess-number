9e34542c7e4fce5cd455831effd45a12b87b6602
Gustavo Lopez <gml.gustavoml@gmail.com>
2025-03-28 00:22:41 -0300

    Update OneSignal SDK integration: add service worker path and import SDK script

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
 self.addEventListener('install', () => {
   console.log('Service Worker instalado');
