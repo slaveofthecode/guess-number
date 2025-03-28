7773ab304b93ccc38bd118a15096aba7d70a7fd3
Gustavo Lopez <gml.gustavoml@gmail.com>
2025-03-27 22:27:38 -0300

    Disable OneSignal notify button in App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       appId: '26b12df5-7d0b-4824-b51e-50ed5471f59f',
       allowLocalhostAsSecureOrigin: true,
       notifyButton: {
-        enable: true,
+        // enable: true,
       },
       serviceWorkerParam: { scope: '/' },
       serviceWorkerPath: './sw.js',
