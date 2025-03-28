f7f4ef011b6260ae47add55ac5e2b142ab148f7e
Gustavo Lopez <gml.gustavoml@gmail.com>
2025-03-28 00:26:49 -0300

    Refactor service worker registration in index.html and comment out registration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-/* eslint-disable no-undef */
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then((reg) => console.log('SW registered:', reg))
-    .catch((err) => console.error('Error registring the SW:', err));
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/sw.js')
+//     .then((reg) => console.log('SW registered:', reg))
+//     .catch((err) => console.error('Error registring the SW:', err));
+// }
 
 createRoot(document.getElementById('root')!).render(
   <>
